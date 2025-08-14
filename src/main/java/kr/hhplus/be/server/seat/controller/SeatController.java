56a79a5332ea21a4e5bfaa349fda78afed3b1d4d
i love milkiss <shykse@naver.com>
2025-08-15 04:13:30 +0900

    [feat] 레디스 캐시 적용 API

--- src/main/java/kr/hhplus/be/server/seat/controller/SeatController.java
+++ src/main/java/kr/hhplus/be/server/seat/controller/SeatController.java
@@ -6,10 +6,8 @@ import kr.hhplus.be.server.seat.domain.ReservationStatus;
 import kr.hhplus.be.server.seat.domain.Seat;
 import kr.hhplus.be.server.seat.domain.Zone;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,16 @@ public class SeatController {
     public Response<List<Seat>> findSeatWhereConcertAndStatus(@RequestParam("concertId") Long concertId, @RequestParam("zone") Zone zone, @RequestParam("status") ReservationStatus status) {
         List<Seat> seatsByZoneAndConcertIdAndStatus = seatService.findSeatsByZoneAndConcertIdAndStatus(concertId, zone, status);
         return Response.ok(seatsByZoneAndConcertIdAndStatus);
+    }
+
+    @GetMapping("/on-cache/{seatId}/concertId/{concertId}")
+    @Cacheable(cacheNames = "seat", key = "'concert:' + #concertId + 'seat:' + #seatId")
+    public Seat getSeat(@PathVariable Long concertId, @PathVariable Long seatId) {
+        return seatService.seatInfo(concertId, seatId);
+    }
+
+    @GetMapping("/off-cache/{seatId}/concertId/{concertId}")
+    public Seat getSeatNoCache(@PathVariable Long concertId, @PathVariable Long seatId) {
+        return seatService.seatInfo(concertId, seatId);
     }
 }
